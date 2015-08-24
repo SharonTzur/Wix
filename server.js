4d486c095686d21223ed82668cfca14d536df976
Sharon <gaytomatoxx@gmail.com>
2015-08-24 19:42:05 +0300

    update port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 
 var app = express();
@@ -13,3 +14,19 @@ app.listen(80, function() {
 console.log("Hello World 2");
 
 
+*/
+
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(80, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
