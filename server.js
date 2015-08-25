1f3238a0b1707b1ccc00b6ad21c99f18f2f9764c
Sharon <gaytomatoxx@gmail.com>
2015-08-25 19:51:29 +0300

    try mongo

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ var mongoose = require('mongoose');
 
 //connecting local mongodb database named test
 var db = mongoose.connect('mongodb://sharontzur:sharontzur@ds055862.mongolab.com:55862/my-db');
-console.log(db.connections.db);
+console.log(db);
 
 //testing connectivity
 mongoose.connection.once('connected', function() {
     console.log("Database connected successfully");
+});
+
+
+app.listen(8080, function() {
+    console.log('ready to go!');
 });
