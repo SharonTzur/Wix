42ed034f6d72524dd0413760f3a613ca74f0ae59
Sharon <gaytomatoxx@gmail.com>
2015-08-25 17:30:25 +0300

    try mongo

--- server.js
+++ server.js
@@ -9,20 +9,28 @@ console.log("Hello World");
 var mongoose = require('mongoose');
 
 //connecting local mongodb database named test
-var uri = 'mongodb://http://52.26.100.183/test';
-var db = mongoose.connect(uri);
+var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 //testing connectivity
 mongoose.connection.once('connected', function() {
     console.log("Database connected successfully");
+    debugger;
 
 });
+/*
 
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+});
 
 app.listen(8080, function() {
     console.log('ready to go!');
 });
 
+*/
 
 
 
