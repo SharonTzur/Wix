59b28d445237515c0b93cf5d185b7c41401f3be8
Sharon <gaytomatoxx@gmail.com>
2015-08-25 09:14:19 +0300

    update port

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ app.use(express.static(__dirname + '/public'));
 
 console.log("Hello World");
 
+var mongoose = require('mongoose');
+
+//connecting local mongodb database named test
+var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+//testing connectivity
+mongoose.connection.once('connected', function() {
+    console.log("Database connected successfully")
+});
+
+
 app.listen(8080, function() {
     console.log('ready to go!');
 });
 
-console.log("Hello World 2");
 
 
 
