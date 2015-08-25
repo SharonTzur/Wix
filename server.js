e976e0b6f8fcda5805c03c671eefc60fcabd2117
Sharon <gaytomatoxx@gmail.com>
2015-08-25 22:53:59 +0300

    try mongo

--- server.js
+++ server.js
@@ -19,6 +19,33 @@ mongoose.connection.once('connected', function() {
 console.log(db);
 
 
+var TodoSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean,
+    note: String,
+    updated_at: { type: Date, default: Date.now },
+});
+
+ var Todo = mongoose.model('Todo', TodoSchema);
+
+
+var todo = new Todo({name: 'Master NodeJS', completed: false, note: 'Getting there...'});
+
+ todo.save(function(err){
+    if(err)
+        console.log(err);
+    else
+        console.log(todo);
+});
+
+
+Todo.find(function (err, todos) {
+    if (err) return console.error(err);
+    console.log(todos)
+});
+
+
+
 app.listen(8080, function() {
     console.log('ready to go!');
 });
