e25e5d17bfc8156628e850c03874f4c1f9890d62
Sharon <gaytomatoxx@gmail.com>
2015-08-25 16:59:21 +0300

    ..

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
 //testing connectivity
 mongoose.connection.once('connected', function() {
     console.log("Database connected successfully");
-    db.shows.find({ title: 'Stella' });
 
 });
 
