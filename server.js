1b61d396e7db3248cb3b676bc0db729c6dfbaaf7
Sharon <gaytomatoxx@gmail.com>
2015-08-25 08:26:10 +0300

    update port

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ console.log("Hello World 2");
 
 */
 
-/*
 
 var express = require('express');
 var app = express();
@@ -25,16 +24,17 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
-});*/
+});
 
+/*
 
 require("http").createServer(function(request, response){
     response.writeHeader(200, {"Content-Type": "text/plain"});
     response.write("Hello World!");
     response.end();
-}).listen(8080);
+}).listen(8080);*/
